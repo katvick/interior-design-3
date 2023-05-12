4e26879fbcbbb1e3e10220814b85ae14a41ced5f
Kate <stolyarova.katy@ya.ru>
2023-05-12 22:54:09 +0500

    добавит анимацию счетчика на числа

--- js/main.js
+++ js/main.js
@@ -29,4 +29,25 @@ $(document).ready(function(){
     },
   });
 
+  // animated
+  let show = true;
+  let countbox = ".animated-number";
+  $(window).on("scroll load resize", function () {
+      if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+      let w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+      let e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+      let w_height = $(window).height(); // Высота окна браузера
+      let d_height = $(document).height(); // Высота всего документа
+      let e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+      if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+          $('.num').css('opacity', '1');
+          $('.num').spincrement({
+              thousandSeparator: "",
+              duration: 2000
+          });
+
+          show = false;
+      }
+  });
+
 });
